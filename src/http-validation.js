d4465e0f3c44f0edf6996243aa63bc8ad0b7344e
Gabriel Prado <guardavege@gmail.com>
2023-12-21 17:00:02 -0300

    Tratando erros

--- src/http-validation.js
+++ src/http-validation.js
@@ -1,24 +1,50 @@
+import chalk from "chalk";
+
 function ExtrairList(list)
 {
     return list.map((Objectlinks) => Object.values(Objectlinks).join());
 }
+
+
+
 
 export default async function GetListUrl(lists)
 {
     
     const links =  ExtrairList(lists);
     const status =  await ValidaUrl(links);
-    return status
+    return lists.map((listaUpdate,indice) => ({
+        ...listaUpdate,
+        Status: status[indice]
+    }));
+}
+
+function TrataErro(erro)
+{
+    if(erro.cause.code === 'ENOTFOUND')
+    {
+    return "Link não encontrado";
+    }else
+    {
+        return "Ocorreu um Erro Por Favor Tente Novamente";
+    }
 }
 
 async function ValidaUrl(ArrayUrl)
 {
+    
     const ArrayPending = await Promise.all
     (
         ArrayUrl.map(async (url) =>
         {
+            try
+            {
             const response = await fetch(url);
             return response.status;
+            }catch (erro)
+            {
+                return TrataErro(erro);
+            }
         })
     )
     return ArrayPending;
